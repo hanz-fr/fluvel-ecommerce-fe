358aee3127e66289bcc32e3284ac3ecd14518da8
recta-rrae <raihanf787@gmail.com>
2023-01-05 16:13:19 +0700

    added signin page

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -74,8 +74,109 @@ class SignInPage extends StatelessWidget {
       );
     }
 
+    Widget passwordInput() {
+      return Container(
+        margin: const EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Password',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium
+              ),
+            ),
+            const SizedBox(height: 12,),
+            Container(
+              height: 50,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: bgColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Row(
+                  children: [
+                    Image.asset('assets/password_icon.png', width: 17,),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        obscureText: true,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Your Password',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    )
+                  ],
+                ) 
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    
+    Widget signInButton() {
+
+      return Container(
+        height: 50,
+        width: double.infinity,
+        margin: const EdgeInsets.only(top: 30),
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            backgroundColor: primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            )
+          ),
+          child: Text(
+            'Sign In',
+            style: primaryTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+
+    }
+
+
+    Widget footer() {
+
+      return Container(
+        margin: const EdgeInsets.only(bottom: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Don\`t have an account?  ',
+              style: subtitleTextStyle.copyWith(
+                fontSize: 12
+              ),
+            ),
+            Text(
+              'Sign Up',
+              style: puprleTextStyle.copyWith(
+                fontSize: 12,
+                fontWeight: medium
+              ),
+            )
+          ],
+        ),
+      );
+
+    }
+
+
     return Scaffold(
       backgroundColor: bgColor1,
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Container(
           margin: EdgeInsets.symmetric(
@@ -86,6 +187,10 @@ class SignInPage extends StatelessWidget {
             children: [
               header(),
               emailInput(),
+              passwordInput(),
+              signInButton(),
+              const Spacer(),
+              footer(),
             ],
           ),
         ),
