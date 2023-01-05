2bfb8c925153e3e2a401a9562b7662d591abf3b8
Han <59479453+recta-rrae@users.noreply.github.com>
2023-01-05 19:07:31 +0700

    added homepage header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,13 +5,59 @@ import 'package:flutter_ecommerce/theme.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  /* Header */
+  Widget header(){
+    return Container(
+      margin: EdgeInsets.only(
+        top: defaultMargin,
+        left: defaultMargin,
+        right: defaultMargin
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Halo, Alex',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                Text(
+                  '@alexander',
+                  style: subtitleTextStyle.copyWith(
+                    fontSize: 16
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 54,
+            height: 54,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/profile_image.png',
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: primaryTextStyle,
-      ),
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
