f5c88377ac44524f0d344dc1b4eea61339dd7147
Han <59479453+recta-rrae@users.noreply.github.com>
2023-01-06 13:19:28 +0700

    added chat input

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -47,9 +47,107 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    /* Product Preview */
+    Widget productPreview() {
+      return Container(
+        width: 225,
+        height: 74,
+        margin: const EdgeInsets.only(bottom: 20),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: bgColor5,
+          border: Border.all(
+            color: primaryColor,
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
+              ),
+            ),
+            const SizedBox(width: 10,),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Court Vision 2.0',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 2,),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 22,
+            ),
+          ],
+        ),
+      );
+    }
+
+    /* Chat Input */
+    Widget chatInput() {
+      return Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: bgColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20,),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: bgColor3,
       appBar: header(),
+      bottomNavigationBar: chatInput(),
     );
   }
 }
